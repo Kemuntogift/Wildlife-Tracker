4ef330e2a789d557af239088ec40bc4cf5efc297
Kemunto Gift <giftoyugi@gmail.com>
2022-05-08 16:14:32 +0300

    add starter code to animal.java and add more files with test classes

---
+++ src/main/java/Animals.java
@@ -0,0 +1,14 @@
+public abstract class Animals {
+    public int id;
+    public String name;
+    public String type;
+    private String health;
+    private String age;
+    public static final String ANIMAL_TYPE="normal";
+
+
+
+    public Animals(String name,String type) {
+
+    }
+}
