3af7967fc6ce45adc96062a987fe37caeda6b054
Kemunto Gift <giftoyugi@gmail.com>
2022-05-09 11:55:20 +0300

    add new rangers table and add setup and teardown methods

--- src/test/java/RangersTest.java
+++ src/test/java/RangersTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RangersTest {
+    @BeforeEach
+    void setUp() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "postgres", "KEMUNTO543210");
+    }
 
+    @AfterEach
+    void tearDown() {
+        try(Connection con = DB.sql2o.open()) {
+            String deleteSightingsQuery = "DELETE FROM rangers *;";
+            con.createQuery(deleteSightingsQuery).executeUpdate();
+        }
+    }
 }
