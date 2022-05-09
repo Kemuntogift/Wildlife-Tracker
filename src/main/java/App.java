7f6e61f059a55596d355c6a6e5538930369f9184
Kemunto Gift <giftoyugi@gmail.com>
2022-05-09 17:08:27 +0300

    add path to get animals from animal-form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,10 +10,14 @@ public class App {
 
     public static void main(String[] args) {
         staticFileLocation("/public");
-
+//landing page
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+
+
+        //get animal from animal-form
+
     }
 }
