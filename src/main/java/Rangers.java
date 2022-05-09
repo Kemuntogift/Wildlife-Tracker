30e0d33c611ac43edc83bb26c66bcf6d80cd088a
Kemunto Gift <giftoyugi@gmail.com>
2022-05-09 12:05:41 +0300

    add instantiating tests for name and badge number

--- src/main/java/Rangers.java
+++ src/main/java/Rangers.java
@@ -1,2 +1,20 @@
 public class Rangers {
+
+    private String name;
+    private int badge_number;
+
+
+    public Rangers(String name, int badge_number) {
+        this.name = name;
+        this.badge_number = badge_number;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBadge_number() {
+        return badge_number;
+    }
 }
--- src/test/java/RangersTest.java
+++ src/test/java/RangersTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
@@ -17,5 +18,15 @@ class RangersTest {
             String deleteSightingsQuery = "DELETE FROM rangers *;";
             con.createQuery(deleteSightingsQuery).executeUpdate();
         }
+    }
+    @Test
+    public void Rangers_instantiatesWithName_String() {
+        Rangers testRangers = new Rangers("Mary",2);
+        assertEquals("Mary", testRangers.getName());
+    }
+    @Test
+    public void Rangers_instantiatesWithBadgeNumber_Integer() {
+        Rangers testRangers = new Rangers("Mary",2);
+        assertEquals(2, testRangers.getBadge_number());
     }
 }
