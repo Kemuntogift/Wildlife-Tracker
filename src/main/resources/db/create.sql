b2df2f4dbe0c2f606898af52e874a3ba2b0a35ee
Kemunto Gift <giftoyugi@gmail.com>
2022-05-09 07:29:14 +0300

    added timestamp to table

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE wildlife_tracker;
 \c wildlife_tracker
-CREATE TABLE animals (id serial PRIMARY KEY, name varchar,type VARCHAR,health VARCHAR,age VARCHAR);
+CREATE TABLE animals (id serial PRIMARY KEY, name varchar,type VARCHAR,health VARCHAR,age VARCHAR),sighted timestamp;
 CREATE DATABASE wildlife_tracker_test WITH TEMPLATE wildlife_tracker;
