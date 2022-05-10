98549be18420ff307cf1fbc2fa892a6859bd48c7
Kemunto Gift <giftoyugi@gmail.com>
2022-05-10 14:15:56 +0300

    edit logic and tests for endangered animals

--- src/main/java/NormalAnimal.java
+++ src/main/java/NormalAnimal.java
@@ -1,5 +1,6 @@
 import org.sql2o.Connection;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -13,6 +14,10 @@ public class NormalAnimal extends Animal implements DatabaseManagement {
         this.name = name;
         this.age = age;
         type = ANIMAL_TYPE;
+        //throw exception if fields are empty
+        if (name.isEmpty() || age.isEmpty()){
+            throw new IllegalArgumentException("Please fill out all fields.");
+        }
     }
 
 
@@ -35,6 +40,36 @@ public class NormalAnimal extends Animal implements DatabaseManagement {
             return animal;
 
         }
+    }
+    @Override
+    public boolean equals(Object otherAnimal){
+        if (!(otherAnimal instanceof NormalAnimal)) {
+            return false;
+        } else {
+            NormalAnimal newAnimal = (NormalAnimal) otherAnimal;
+            return this.getName().equals(newAnimal.getName()) &&
+                    this.getHealth().equals(newAnimal.getHealth()) &&
+                    this.getAge().equals(newAnimal.getAge());
+        }
+    }
 
+    public static List<Object> getAnimals() {
+        List<Object> allAnimals = new ArrayList<Object>();
+
+        try(Connection connection = DB.sql2o.open()) {
+            String sqlNormal = "SELECT * FROM animals WHERE id=:id AND type='normal';";
+            List<NormalAnimal> normalAnimals = connection.createQuery(sqlNormal)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(NormalAnimal.class);
+            allAnimals.addAll(normalAnimals);
+
+            String sqlEndangered = "SELECT * FROM animals WHERE id=:id AND type='endangered';";
+            List<EndangeredAnimal> endangeredAnimals = connection.createQuery(sqlEndangered)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(EndangeredAnimal.class);
+            allAnimals.addAll(endangeredAnimals);
+        }
+
+        return allAnimals;
     }
 }
