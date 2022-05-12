d795f3aae7b3695860c87a7e03d9142591c19c00
Kemunto Gift <giftoyugi@gmail.com>
2022-05-12 14:29:27 +0300

    change links and fix heroku errors

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,8 +1,9 @@
 import org.sql2o.*;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "postgres", "KEMUNTO543210");
-// heroku database credentials
-//    String connectionString = "jdbc:postgresql://ec2-3-224-164-189.compute-1.amazonaws.com:5432/db43k9ldso3p7f"; //!
-//    Sql2o sql2o = new Sql2o(connectionString, "adnqphfufcerat", "5a299a34fea7f2b331b0c17e797bd96312fa95e91a21ccee1f4ae443aaa4be7c"); //!
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "postgres", "KEMUNTO543210");
+//// heroku database credentials
+static String connectionString = "jdbc:postgresql://ec2-3-224-164-189.compute-1.amazonaws.com:5432/db43k9ldso3p7f"; //!
+    static Sql2o sql2o = new Sql2o(connectionString, "adnqphfufcerat", "5a299a34fea7f2b331b0c17e797bd96312fa95e91a21ccee1f4ae443aaa4be7c"); //!
 }
+
